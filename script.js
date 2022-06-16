1414a5cbe412dcf4dcc2adfc401414d1fe827480
Weiller CArvalho <engweiller@hotmail.com>
2022-06-16 18:52:03 -0300

    finished50%ofjavascript

--- script.js
+++ script.js
@@ -1,165 +1,75 @@
-
-
 let amount;
-let deck = [];
-let count = 0;
 let virada;
 let vira1;
 let vira2;
 let puxar;
+let cards;
+let value;
+let code;
+const deck = [
+    `img1.gif`,
+    `img2.gif`,
+    `img3.gif`,
+    `img4.gif`,
+    `img5.gif`,
+    `img6.gif`,
+    `img7.gif`,
+];
 
-function quantidade() {
-    amount = Number(prompt(`Com quantas cartas você deseja jogar entre 4 e 14(escolha apenas valores pares)?`));
-    while (amount % 2 === 1) {
-        amount = Number(prompt(`Digite apenas valores pares:`));}
-    while (isNaN(amount) === true) {
-        amount = Number(prompt(`Caracter invalido, digite um valor par.`));
-    }
-    while (amount % 2 === 1) {
-        amount = Number(prompt(`Digite apenas valores pares:`));}
-    while (isNaN(amount) === true) {
-        amount = Number(prompt(`Caracter invalido, digite um valor par.`));
-    }
+// function quantidade() {
+//     amount = Number(prompt(`Com quantas cartas você deseja jogar entre 4 e 14(escolha apenas valores pares)?`));
+//     while (amount % 2 === 1) {
+//         amount = Number(prompt(`Digite apenas valores pares:`));}
+//     while (isNaN(amount) === true) {
+//         amount = Number(prompt(`Caracter invalido, digite um valor par.`));
+//     }
+//     while (amount % 2 === 1) {
+//         amount = Number(prompt(`Digite apenas valores pares:`));}
+//     while (isNaN(amount) === true) {
+//         amount = Number(prompt(`Caracter invalido, digite um valor par.`));
+//     }
 
-    return amount;
+//     return amount;
+// }
+// quantidade();
+
+// deck.length = amount;
+
+value = document.getElementById(`valores`);
+code = ``;
+deck.forEach(index =>{
+    code += `
+        <div class="imagesimages">
+            <img class="frente-face" src="images/${index}">
+            <img class="verso-face" src="images/parrotfather.png">
+        </div>`
+});
+value.innerHTML = code + code;
+cards = document.querySelectorAll(`.imagesimages`);
+
+function virar() {
+    this.classList.add(`virar`);
 }
 
-quantidade();
+cards.forEach(chamar => chamar.addEventListener(`click`,virar))
 
 
-function numeroCartas() {
-    while (count < amount) {
-        puxar = document.querySelector(`.sumir`)
-        puxar.classList.remove(`sumir`)
-        count ++;    
-    }
-}
-numeroCartas();
 
-function virar (elemento) {
-    vira1 = document.querySelector(`.primeiropar .face.um`);
-    if (vira1 != null) {
-        vira1.classList.add(`verso`);
-    }else if (vira1 == null) {
-        virada = elemento.classList.add(`frente`);
-    }
 
-}
-function virar2 (elemento) {
-    vira2 = document.querySelector(`.primeiropar .face.dois`);
-    if (vira2 != null) {
-        vira2.classList.add(`verso`);
-    }else if (vira2 == null) {
-        virada2 = elemento.classList.add(`frente`);
-    }
 
-}
-function virar3 (elemento) {
-    vira1 = document.querySelector(`.segundopar .face.um`);
-    if (vira1 != null) {
-        vira1.classList.add(`verso`);
-    }else if (vira1 == null) {
-        virada = elemento.classList.add(`frente`);
-    }
 
-}
-function virar4 (elemento) {
-    vira2 = document.querySelector(`.segundopar .face.dois`);
-    if (vira2 != null) {
-        vira2.classList.add(`verso`);
-    }else if (vira2 == null) {
-        virada2 = elemento.classList.add(`frente`);
-    }
 
-}
-function virar5 (elemento) {
-    vira1 = document.querySelector(`.terceiropar .face.um`);
-    if (vira1 != null) {
-        vira1.classList.add(`verso`);
-    }else if (vira1 == null) {
-        virada = elemento.classList.add(`frente`);
-    }
 
-}
-function virar6 (elemento) {
-    vira2 = document.querySelector(`.terceiropar .face.dois`);
-    if (vira2 != null) {
-        vira2.classList.add(`verso`);
-    }else if (vira2 == null) {
-        virada2 = elemento.classList.add(`frente`);
-    }
 
-}
-function virar7 (elemento) {
-    vira1 = document.querySelector(`.quartopar .face.um`);
-    if (vira1 != null) {
-        vira1.classList.add(`verso`);
-    }else if (vira1 == null) {
-        virada = elemento.classList.add(`frente`);
-    }
 
-}
-function virar8 (elemento) {
-    vira2 = document.querySelector(`.quartopar .face.dois`);
-    if (vira2 != null) {
-        vira2.classList.add(`verso`);
-    }else if (vira2 == null) {
-        virada2 = elemento.classList.add(`frente`);
-    }
 
-}
-function virar9 (elemento) {
-    vira1 = document.querySelector(`.quintopar .face.um`);
-    if (vira1 != null) {
-        vira1.classList.add(`verso`);
-    }else if (vira1 == null) {
-        virada = elemento.classList.add(`frente`);
-    }
 
-}
-function virar10 (elemento) {
-    vira2 = document.querySelector(`.quintopar .face.dois`);
-    if (vira2 != null) {
-        vira2.classList.add(`verso`);
-    }else if (vira2 == null) {
-        virada2 = elemento.classList.add(`frente`);
-    }
 
-}
-function virar11 (elemento) {
-    vira1 = document.querySelector(`.sextoopar .face.um`);
-    if (vira1 != null) {
-        vira1.classList.add(`verso`);
-    }else if (vira1 == null) {
-        virada = elemento.classList.add(`frente`);
-    }
 
-}
-function virar12 (elemento) {
-    vira2 = document.querySelector(`.sextopar .face.dois`);
-    if (vira2 != null) {
-        vira2.classList.add(`verso`);
-    }else if (vira2 == null) {
-        virada2 = elemento.classList.add(`frente`);
-    }
 
-}
-function virar13 (elemento) {
-    vira1 = document.querySelector(`.setimopar .face.um`);
-    if (vira1 != null) {
-        vira1.classList.add(`verso`);
-    }else if (vira1 == null) {
-        virada = elemento.classList.add(`frente`);
-    }
 
-}
-function virar14 (elemento) {
-    vira2 = document.querySelector(`.setimopar .face.dois`);
-    if (vira2 != null) {
-        vira2.classList.add(`verso`);
-    }else if (vira2 == null) {
-        virada2 = elemento.classList.add(`frente`);
-    }
 
-}
-// CRIA UMA LISTA COM ISSO:
+
+
+
+
